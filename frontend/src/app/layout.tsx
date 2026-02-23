ad2828a9f641bc1135b36b6c8e71f0cdab6c4e61
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2026-02-23 09:11:11 +0530

    added analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           <ThemeToggle />
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
